eb85b57ab9e57e01a8b0ada601c064c7e678e329
ilyachase <LyWehobain7>
2017-06-28 22:21:45 +0500

    fb access token

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,9 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 
 class SiteController extends Controller
 {
@@ -20,19 +23,19 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout'],
+                'only'  => [ 'logout' ],
                 'rules' => [
                     [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
+                        'actions' => [ 'logout' ],
+                        'allow'   => true,
+                        'roles'   => [ '@' ],
                     ],
                 ],
             ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
+            'verbs'  => [
+                'class'   => VerbFilter::className(),
                 'actions' => [
-                    'logout' => ['post'],
+                    'logout' => [ 'post' ],
                 ],
             ],
         ];
@@ -44,11 +47,11 @@ class SiteController extends Controller
     public function actions()
     {
         return [
-            'error' => [
+            'error'   => [
                 'class' => 'yii\web\ErrorAction',
             ],
             'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
+                'class'           => 'yii\captcha\CaptchaAction',
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
         ];
@@ -61,7 +64,46 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        if ( !Yii::$app->session->isActive )
+        {
+            Yii::$app->session->open();
+        }
+
+        $fb = new Facebook( [
+            'app_id'     => '109433556350716',
+            'app_secret' => '22e9f275cbca86c39b8f51af404ae779',
+        ] );
+
+        $helper = $fb->getRedirectLoginHelper();
+
+        if ( !Yii::$app->session['facebook_access_token'] )
+        {
+            $helper = $fb->getRedirectLoginHelper();
+            try
+            {
+                Yii::$app->session['facebook_access_token'] = (string) $helper->getAccessToken();
+            }
+            catch ( FacebookResponseException $e )
+            {
+                // TODO: When Graph returns an error
+                //echo 'Graph returned an error: ' . $e->getMessage();
+                //exit;
+            }
+            catch ( FacebookSDKException $e )
+            {
+                // TODO: When validation fails or other local issues
+                //echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                //exit;
+            }
+        }
+
+        if ( !Yii::$app->session['facebook_access_token'] )
+        {
+            $loginUrl = $helper->getLoginUrl( Yii::$app->getRequest()->absoluteUrl, [ 'ads_management' ] );
+            return $this->render( 'fb_login', ['loginUrl' => $loginUrl] );
+        }
+
+        return $this->render( 'index' );
     }
 
     /**
@@ -71,17 +113,19 @@ class SiteController extends Controller
      */
     public function actionLogin()
     {
-        if (!Yii::$app->user->isGuest) {
+        if ( !Yii::$app->user->isGuest )
+        {
             return $this->goHome();
         }
 
         $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+        if ( $model->load( Yii::$app->request->post() ) && $model->login() )
+        {
             return $this->goBack();
         }
-        return $this->render('login', [
+        return $this->render( 'login', [
             'model' => $model,
-        ]);
+        ] );
     }
 
     /**
@@ -104,14 +148,15 @@ class SiteController extends Controller
     public function actionContact()
     {
         $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
+        if ( $model->load( Yii::$app->request->post() ) && $model->contact( Yii::$app->params['adminEmail'] ) )
+        {
+            Yii::$app->session->setFlash( 'contactFormSubmitted' );
 
             return $this->refresh();
         }
-        return $this->render('contact', [
+        return $this->render( 'contact', [
             'model' => $model,
-        ]);
+        ] );
     }
 
     /**
@@ -121,6 +166,6 @@ class SiteController extends Controller
      */
     public function actionAbout()
     {
-        return $this->render('about');
+        return $this->render( 'about' );
     }
 }
---
+++ views/site/fb_login.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Facebook Login';
+?>
+
+<div class="page-content">
+    <p class="bg-warning">
+        Для того, чтобы работать с системой, вам сначала нужно <a href="<?= $loginUrl ?>" target="_blank">войти через Facebook</a>.
+    </p>
+</div>
