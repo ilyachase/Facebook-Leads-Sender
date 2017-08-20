f5f0ebec77ca984ebb049e4a593fc93969c83ab7
ilyachase <LyWehobain7>
2017-08-20 21:07:05 +0500

    update views fix

--- views/connections/update.php
+++ views/connections/update.php
@@ -6,9 +6,9 @@
 /* @var array $destinationsDropdownItems */
 /* @var array $rulesetsDropdownItems */
 
-$this->title = 'Update Connections: ' . $model->id;
+$this->title = 'Update Connection: ' . $model->name;
 $this->params['breadcrumbs'][] = [ 'label' => 'Connections', 'url' => [ 'index' ] ];
-$this->params['breadcrumbs'][] = [ 'label' => $model->id, 'url' => [ 'view', 'id' => $model->id ] ];
+$this->params['breadcrumbs'][] = [ 'label' => $model->name, 'url' => [ 'view', 'id' => $model->id ] ];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="connections-update">
--- views/destinations/update.php
+++ views/destinations/update.php
@@ -4,7 +4,7 @@
 /* @var $model app\models\Activerecord\Destinations */
 /* @var array $clientsDropdownItems */
 
-$this->title = 'Update Destinations: ' . $model->name;
+$this->title = 'Update Destination: ' . $model->name;
 $this->params['breadcrumbs'][] = [ 'label' => 'Destinations', 'url' => [ 'index' ] ];
 $this->params['breadcrumbs'][] = [ 'label' => $model->name, 'url' => [ 'view', 'id' => $model->id ] ];
 $this->params['breadcrumbs'][] = 'Update';
@@ -12,8 +12,10 @@ $this->params['breadcrumbs'][] = 'Update';
 <div class="destinations-update">
 
     <?= $this->render( '_form', [
-        'model' => $model,
-        'clientsDropdownItems' => $clientsDropdownItems,
+        'model'                     => $model,
+        'clientsDropdownItems'      => $clientsDropdownItems,
+        'destinationsDropdownItems' => [],
+        'ruleset_id'                => null,
     ] ) ?>
 
 </div>
